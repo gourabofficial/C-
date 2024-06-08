0c6f00e383e5d075db680271dcd408275562f776
Gourab Ganguly <163894732+gourabofficial@users.noreply.github.com>
2024-06-08 20:14:48 +0530

    Add files via upload

---
+++ c++/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(){
+    int a;
+    int b;
+    std::cout<<"Enter your first number: ";
+    std::cin>>a;
+    std::cout<<"Enter your second number: ";
+    std::cin>>b;
+    int sum = a+b;
+    std::cout<<"Sum of two number : " <<sum;
+    return 0;
+
+}
